ac01cbb447279962769c5950abff1162256c6a1e
Thomas Garrood <thomas@sandboxdigital.com.au>
2017-02-16 20:20:14 +1100

    Fix Tumblr icon

---
+++ src/js/core/curator/filter.js
@@ -0,0 +1,150 @@
+/**
+* ==================================================================
+* Popup
+* ==================================================================
+*/
+
+
+Curator.Popup = function (json,feed) {
+    this.init(json,feed);
+};
+
+
+
+$.extend(Curator.Popup.prototype, {
+    templateId:'#popup-template',
+    videoPlaying:false,
+
+    init: function (popupManager, post, feed) {
+        Curator.log("Popup->init ");
+ 
+        this.popupManager = popupManager;
+        this.json = post.json;
+        this.feed = feed;
+
+        this.$popup = Curator.Template.render(this.templateId, this.json);
+
+        if (this.json.video && this.json.video.indexOf('youtu') >= 0 )
+        {
+            // youtube
+            this.$popup.find('video').remove();
+
+            var youTubeId = Curator.StringUtils.youtubeVideoId(this.json.video);
+
+            var src = '<iframe id="ytplayer" type="text/html" width="615" height="615" \
+            src="https://www.youtube.com/embed/'+youTubeId+'?autoplay=0&rel=0&showinfo" \
+            frameborder="0"></iframe>';
+
+            this.$popup.find('.crt-video-container img').remove();
+            this.$popup.find('.crt-video-container a').remove();
+            this.$popup.find('.crt-video-container').append(src);
+
+        } else if (!this.json.image) {
+            this.$popup.addClass('no-image');
+        }
+
+        if (this.json.video) {
+            this.$popup.addClass('has-video');
+        }
+
+        this.$popup.on('click',' .crt-close', $.proxy(this.onClose,this));
+        this.$popup.on('click',' .crt-previous', $.proxy(this.onPrevious,this));
+        this.$popup.on('click',' .crt-next', $.proxy(this.onNext,this));
+        this.$popup.on('click',' .crt-play', $.proxy(this.onPlay,this));
+
+    },
+
+    onClose: function (e) {
+        e.preventDefault();
+        var that = this;
+        this.hide(function(){
+            that.popupManager.onClose();
+        });
+    },
+
+    onPrevious: function (e) {
+        e.preventDefault();
+
+        this.popupManager.onPrevious();
+    },
+
+    onNext: function (e) {
+        e.preventDefault();
+
+        this.popupManager.onNext();
+    },
+
+    onPlay: function (e) {
+        Curator.log('Popup->onPlay');
+        e.preventDefault();
+
+        this.videoPlaying = !this.videoPlaying;
+
+        if (this.videoPlaying) {
+            this.$popup.find('video')[0].play();
+            this.popupManager.client.track('video:play');
+        } else {
+            this.$popup.find('video')[0].pause();
+            this.popupManager.client.track('video:pause');
+        }
+
+        Curator.log(this.videoPlaying);
+
+        this.$popup.toggleClass('video-playing',this.videoPlaying );
+    },
+
+    show: function () {
+        //
+        // var post = this.json;
+        // var mediaUrl = post.image,
+        //     text = post.text;
+        //
+        // if (mediaUrl) {
+        //     var $imageWrapper = that.$el.find('div.main-image-wrapper');
+        //     this.loadMainImage(mediaUrl, $imageWrapper, ['main-image']);
+        // }
+        //
+        // var $socialIcon = this.$el.find('.social-icon');
+        // $socialIcon.attr('class', 'social-icon');
+        // $socialIcon.addClass(Curator.SOURCE_TYPES[post.sourceType]);
+        //
+        // //format the date
+        // var date = Curator.Utils.dateAsDayMonthYear(post.sourceCreateAt);
+        //
+        // this.$el.find('input.discovery-id').val(post.id);
+        // this.$el.find('div.full-name span').html(post.user_full_name);
+        // this.$el.find('div.username span').html('@' + post.user_screen_name);
+        // this.$el.find('div.date span').html(date);
+        // this.$el.find('div.love-indicator span').html(post.loves);
+        // this.$el.find('div.side-text span').html(text);
+        //
+        // this.wrapper.show();
+        this.$popup.fadeIn(function () {
+            // that.$popup.find('.crt-popup').animate({width:950}, function () {
+            //     $('.popup .content').fadeIn('slow');
+            // });
+        });
+    },
+    
+    hide: function (callback) {
+        Curator.log('Popup->hide');
+        var that = this;
+        this.$popup.fadeOut(function(){
+            that.destroy();
+            callback ();
+        });
+    },
+    
+    destroy: function () {
+        if (this.$popup && this.$popup.length) {
+            this.$popup.remove();
+
+            if (this.$popup.find('video').length) {
+                this.$popup.find('video')[0].pause();
+
+            }
+        }
+
+        delete this.$popup;
+    }
+});
