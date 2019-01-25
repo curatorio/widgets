a8c4d2f78988f51c48aaf09acbe7a95f626e86ec
Thomas Garrood <thomas@sandbox.digital>
2019-01-24 18:49:13 -0800

    Move fonts to a seperate library

--- gulpfile.js
+++ gulpfile.js
@@ -77,9 +77,14 @@ const gulp = require('gulp'),
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // CSS / SCSS
 
+const sassConfig = {
+    style: 'expanded',
+    includePaths: ['node_modules']
+};
+
 gulp.task('styles', () =>  {
     return gulp.src(srcScss+'*.scss')
-        .pipe(sass({ style: 'expanded' }).on('error', sass.logError))
+        .pipe(sass(sassConfig).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(destCss))
         .pipe(notify({ message: 'Styles task complete' }));
@@ -90,7 +95,7 @@ gulp.task('styles', () =>  {
 
 gulp.task('styles:prod', () =>  {
     return gulp.src(srcScss+'*.scss')
-        .pipe(sass({ style: 'expanded' }).on('error', sass.logError))
+        .pipe(sass(sassConfig).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(destCss))
         .pipe(rename({suffix: '.min'}))
